4657851c17ecc4b96d4a99aa5a84ade8fe4e0aa7
zippingsugar <zippingsugar@gmail.com>
2025-10-25 18:01:52 +0200

    minor

--- cluster/submit_job_slurm.sh
+++ cluster/submit_job_slurm.sh
@@ -15,8 +15,8 @@ logfile="slurm-${timestamp}.out"
 cat <<EOT > job.sh
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH --cpus-per-task=96
-#SBATCH --gpus=rtx_3090:6
+#SBATCH --cpus-per-task=64
+#SBATCH --gpus=rtx_3090:1
 #SBATCH --time=71:00:00
 #SBATCH --mem-per-cpu=4048
 #SBATCH --output=$logfile
